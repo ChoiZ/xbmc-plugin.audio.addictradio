d4f13ba75804be56cd0b9c069a20a36f34fe09d4
François LASSERRE <choiz@me.com>
2014-06-29 14:57:21 +0200

    Update addon.py
    Update stream url.

--- addon.py
+++ addon.py
@@ -5,11 +5,11 @@ plugin = Plugin()
 @plugin.route('/')
 def main_menu():
     items = [
-        {'label': plugin.get_string(30000), 'path': "http://stream9.addictradio.net/addictalternative", 'is_playable': True},
-        {'label': plugin.get_string(30001), 'path': "http://stream9.addictradio.net/addictlounge", 'is_playable': True},
-        {'label': plugin.get_string(30002), 'path': "http://stream9.addictradio.net/addictrock", 'is_playable': True},
-        {'label': plugin.get_string(30003), 'path': "http://stream9.addictradio.net/addictstar", 'is_playable': True},
-        {'label': plugin.get_string(30004), 'path': "http://stream9.addictradio.net/addictldg", 'is_playable': True},
+        {'label': plugin.get_string(30000), 'path': "http://stream1.addictradio.net/addictalternative", 'is_playable': True},
+        {'label': plugin.get_string(30001), 'path': "http://stream1.addictradio.net/addictlounge", 'is_playable': True},
+        {'label': plugin.get_string(30002), 'path': "http://stream1.addictradio.net/addictrock", 'is_playable': True},
+        {'label': plugin.get_string(30003), 'path': "http://stream1.addictradio.net/addictstar", 'is_playable': True},
+        {'label': plugin.get_string(30004), 'path': "http://stream1.addictradio.net/addictldg", 'is_playable': True},
     ]
 
     return items
